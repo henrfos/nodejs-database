72c0f73606a87c816f8c4826df08660eddb04cac
henrfos <henrik.fossing17@gmail.com>
2024-04-16 21:57:00 +0200

    users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let users = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf-8'));
+  res.render('users', { users: users });
 });
 
 module.exports = router;
